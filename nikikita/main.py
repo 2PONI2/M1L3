85bc046aaa0d5fdf55945197aa0a6375fa7f4778
2PONI2 <114856339+2PONI2@users.noreply.github.com>
2024-04-21 10:21:06 +0800

    Add files via upload

---
+++ nikikita/main.py
@@ -0,0 +1,41 @@
+import telebot 
+from config import token 
+ 
+bot = telebot.TeleBot(token) 
+ 
+ 
+# Handle '/start' and '/help' 
+@bot.message_handler(commands=['start']) 
+def send_welcome(message): 
+    bot.reply_to(message, """\ 
+Hi there, I am EchoBot. 
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\ 
+""") 
+ 
+@bot.message_handler(commands=['help']) 
+def send_help(message): 
+    bot.reply_to(message, """\ 
+                Пиши текст, там в коде команды подсказа 
+                ;)\ 
+""") 
+ 
+@bot.message_handler(commands=['fact']) 
+def send_fact(message): 
+    bot.reply_to(message, """\ 
+            На Байкале утонуло сотни тонн чистого золота и их никто не нашел по сей день\ 
+""") 
+ 
+@bot.message_handler(commands=['joke']) 
+def send_joke(message): 
+    bot.reply_to(message, """\ 
+            Летит ворона и видит провода, хочет сесть 
+садится, а они оказались огаленные, получился электро-CAR\ 
+""") 
+     
+# Handle all other messages with content_type 'text' (content_types defaults to ['text']) 
+@bot.message_handler(func=lambda message: True) 
+def echo_message(message): 
+    bot.reply_to(message, message.text) 
+ 
+ 
+bot.infinity_polling()
