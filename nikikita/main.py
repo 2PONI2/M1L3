15476ff422b61ffdfc98da9299692e70859a5c48
2PONI2 <114856339+2PONI2@users.noreply.github.com>
2024-04-21 10:22:19 +0800

    Update main.py

--- nikikita/main.py
+++ nikikita/main.py
@@ -19,6 +19,11 @@ def send_help(message):
                 ;)\ 
 """) 
  
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+ 
 @bot.message_handler(commands=['fact']) 
 def send_fact(message): 
     bot.reply_to(message, """\ 
